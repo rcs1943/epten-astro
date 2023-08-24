0082b12d7c86a038a54520216f8b123a1d5e461b
Ralf <kerwints6@gmail.com>
2023-08-24 03:02:25 -0500

    Agregados:     * Componente de Sobre Epten.     * Componente Misión Visión.

---
+++ src/constants/nosotros.ts
@@ -0,0 +1,29 @@
+export const PRESENTATION_BOX = {
+    content: "Empresa formada por Profesionales en Pedagogía, Sistemas, Industriales y Profesionales Técnicos con una amplia trayectoria de más de 20 años en el campo comercial-industrial-servicios. Creada para fomentar la tecnología y los negocios a fin de transformar la vida de las personas y la de su comunidad.",
+    cardList: [
+        {
+            iconPath: "/images/nosotros/ICON_EXPERIENCE.svg",
+        },
+        {
+            iconPath: "/images/nosotros/ICON_EXPERIENCE.svg",
+        },
+        {
+            iconPath: "/images/nosotros/ICON_EXPERIENCE.svg",
+        },
+        {
+            iconPath: "/images/nosotros/ICON_EXPERIENCE.svg",
+        },
+    ]
+};
+
+export const MISSION = {
+    topTitle: "NUESTRA",
+    bottomTitle: "MISIÓN",
+    content: "Brindar una instrucción de calidad orientada al emprendimiento y poder satisfacer ampliamente las necesidades de aprendizaje de nuestros estudiantes.",
+}
+
+export const VISION = {
+    topTitle: "NUESTRA",
+    bottomTitle: "VISIÓN",
+    content: "Ser una institución líder en educación, transformando la vida de sus estudiantes y la comunidad.",
+}
