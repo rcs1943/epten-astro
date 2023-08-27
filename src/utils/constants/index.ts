877fccdbc11df46883733c2394d8cb21ca04c02c
Ralf <kerwints6@gmail.com>
2023-08-27 00:44:26 -0500

    Sección cursos del index

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -3,12 +3,35 @@ export const PROMO_COURSES = [
         courseName: "EXCEL BÁSICO",
         schedule: "Miércoles y viernes de 07:00PM a 10:00PM",
         startDate: "08/03/2023",
-        imagePath: "/images/promo/PROMO_EXCEL.jpg"
+        imagePath: "/images/promo/PROMO_EXCEL.jpg",
     },
     {
         courseName: "PYTHON FUNDAMENTALS",
         schedule: "Lunes y jueves de 07:00PM a 10:00PM",
         startDate: "08/03/2023",
-        imagePath: "/images/promo/PROMO_PYTHON.jpg"
+        imagePath: "/images/promo/PROMO_PYTHON.jpg",
     },
-]
+];
+
+export const CATEGORY_CARDS = [
+    {
+        title: "Tecnologías de la información",
+        linkPath: "./cursos/tecnologias-de-la-informacion",
+        imgPath: "/images/cursos/CATEGORY_IT.jpg",
+    },
+    {
+        title: "Diseño",
+        linkPath: "./cursos/diseño",
+        imgPath: "/images/cursos/CATEGORY_DESIGN.jpg",
+    },
+    {
+        title: "Negocios",
+        linkPath: "./cursos/negocios",
+        imgPath: "/images/cursos/CATEGORY_BUSINESS.jpg",
+    },
+    {
+        title: "Ofimática",
+        linkPath: "./cursos/ofimatica",
+        imgPath: "/images/cursos/CATEGORY_OFIMATICA.jpg",
+    },
+];
